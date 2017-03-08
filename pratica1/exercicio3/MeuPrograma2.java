fbef15cd4de91fa7b3da5da7f5855212495b05f6
Victor Rampanha Nicolaus <victor_nicolaus@hotmail.com>
2017-03-08 09:59:31 -0300

    ex3

---
+++ pratica1/exercicio3/MeuPrograma2.java
@@ -0,0 +1,10 @@
+//Victor Rampanha Nicolaus
+//Ra:14.01617-6
+
+class MeuProgama {
+  public static void main (String[] args) {
+             //Mensagem de saida do sistema
+             System.out.println("Olá mundo!");
+             System.out.println("Estou programando no modo Conan.:)");
+  }
+}
